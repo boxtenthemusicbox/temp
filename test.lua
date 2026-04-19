8ed132124064a2a92abd1009cc25682ad373c970
Boxten <boxtenkeyes@gmail.com>
2026-04-19 16:04:55 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -10,8 +10,8 @@
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
 -- yo im dumb
-if getgenv().STOP_SPAM_EXECUTING_BSGUI then return end
-getgenv().STOP_SPAM_EXECUTING_BSGUI = true
+if getgenv().STOP_SPAM_EXECUTING_TBSGUI then return end
+getgenv().STOP_SPAM_EXECUTING_TBSGUI = true
 
 -------------------------------------------------------------------------------------------------------------------------------
 
